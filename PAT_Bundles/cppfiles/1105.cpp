3a500fee83caacde84bcc3c7e1c31587496d0fd0
Bangchao Yu <yubc0321@gmail.com>
2017-03-02 23:02:06 +0800

    update

--- PAT_Bundles/cppfiles/1105.cpp
+++ PAT_Bundles/cppfiles/1105.cpp
@@ -1,51 +1,48 @@
-#include <cmath>
-#include <vector>
 #include <cstdio>
 #include <algorithm>
+#include <cmath>
+#include <vector>
 using namespace std;
-int func(int N) {
-  int i = sqrt((double)N);
-  while(i >= 1) {
-      if(N % i == 0)
-          return i;
-      i--;
+int getn(int num) {
+  int n = sqrt(num * 1.0);
+  while(num % n != 0) {
+    n--;
   }
-  return 1;
+  return n;
 }
-
-int cmp(int a, int b) {return a > b;}
-
+bool cmp(int a, int b) {
+  return a > b;
+}
 int main() {
-  int N, m, n, t = 0;
+  int N, m, n;
+  vector<int> v;
   scanf("%d", &N);
-  n = func(N);
-  m = N / n;
-  vector<int> a(N);
-  for (int i = 0; i < N; i++)
-      scanf("%d", &a[i]);
-  sort(a.begin(), a.end(), cmp);
-  vector<vector<int> > b(m, vector<int>(n));
-  int level = m / 2 + m % 2;
-  for (int i = 0; i < level; i++) {
-    //向右
+  v.resize(N);
+  for(int i = 0; i < N; i++)
+    scanf("%d", &v[i]);
+  sort(v.begin(), v.end(), cmp);
+  n = getn(N);  //列
+  m = N / n;  //行
+  int ans[m][n];
+  int t = 0;
+  for(int i = 0; i < (m / 2 + m % 2); i++) {
     for(int j = i; j <= n - 1 - i && t <= N - 1; j++)
-      b[i][j] = a[t++];
-    //向下
-    for(int j = i + 1; j <= m - 1 - i - 1 && t <= N - 1; j++)
-      b[j][n - 1 - i] = a[t++];
-    //向左
-    for(int j = n - 1 - i; j >= i && t <= N - 1; j--)
-      b[m - 1 - i][j] = a[t++];
-    //向上
-    for(int j = m - 1 - i - 1; j >= i + 1 && t <= N - 1; j--)
-      b[j][i] = a[t++];
+      ans[i][j] = v[t++];
+    for(int j = i + 1; j <= m - 2 - i && t <= N - 1; j++)
+      ans[j][n - i - 1] = v[t++];
+    for(int j = n - i - 1; j >= i && t <= N - 1; j--)
+      ans[m - i - 1][j] = v[t++];
+    for(int j = m - i - 2; j >= i + 1 && t <= N - 1; j--)
+      ans[j][i] = v[t++];
   }
-  for (int i = 0; i < m; i++) {
-      for (int j = 0 ; j < n; j++) {
-          printf("%d", b[i][j]);
-          if (j != n - 1) printf(" ");
-      }
-      printf("\n");
+  for(int i = 0; i < m; i++) {
+    for(int j = 0; j < n; j++) {
+      if(j == 0)
+        printf("%d", ans[i][j]);
+      else
+        printf(" %d", ans[i][j]);
+    }
+    printf("\n");
   }
   return 0;
 }
--- PAT_Bundles/cppfiles/1106.cpp
+++ PAT_Bundles/cppfiles/1106.cpp
@@ -2,39 +2,41 @@
 #include <vector>
 #include <cmath>
 using namespace std;
-vector<int> v[100005];
-vector<int> path;
+int n;
+double p, r, ans;
+vector<int> v[100010];
 int mindepth = 99999999;
-int count = 1;
-double p, r;
-void dfs(int x, int depth) {
-  //path.push_back(x);
-  //为了避免超时，先比较depth
-  if(mindepth < depth)
-    return ;
-  if(v[x].size() == 0) {
-    if(mindepth == depth)
-      count++;
-    if(mindepth > depth) {
+int t = 0;
+void dfs(int node, int depth) {
+  if(v[node].size() == 0) {
+    if(depth < mindepth) {
       mindepth = depth;
-      count = 1;
+      t = 1;
+    } else if(depth == mindepth) {
+      t++;
     }
     return ;
   }
-  for(int i = 0; i < v[x].size(); i++)
-    dfs(v[x][i], depth + 1);
+  for(int i = 0; i < v[node].size(); i++) {
+    dfs(v[node][i], depth + 1);
+  }
 }
 int main() {
-  int n, k, temp;
   scanf("%d %lf %lf", &n, &p, &r);
+  r = r / 100;
+  int k, temp;
   for(int i = 0; i < n; i++) {
     scanf("%d", &k);
-    for(int j = 0; j < k; j++) {
-      scanf("%d", &temp);
-      v[i].push_back(temp);
+    if(k == 0)
+      continue;
+    else {
+      for(int j = 0; j < k; j++) {
+        scanf("%d", &temp);
+        v[i].push_back(temp);
+      }
     }
   }
   dfs(0, 0);
-  printf("%.4f %d", p * pow(1 + r/100, mindepth), count);
+  printf("%.4lf %d", p * pow((1 + r), mindepth), t);
   return 0;
 }
