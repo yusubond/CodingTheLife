8426ada5a0421094049f2b4c929f71aed57b4134
Bangchao Yu <yubc0321@gmail.com>
2017-03-02 11:32:17 +0800

    update

--- my.cpp
+++ my.cpp
@@ -1,57 +1,92 @@
 #include <cstdio>
 #include <vector>
-#include <queue>
+#include <map>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-struct node {
-  int id;
-  int layer;
-};
-int n, l, m, k;
-vector<vector<int> > v;
-int dfs(node x) {
-  bool visit[1010] = {false};
-  queue<node> q;
-  q.push(x);
-  visit[x.id] = true;
-  int count = 0;
-  while(!q.empty()) {
-    node top = q.front();
-    q.pop();
-    int topid = top.id;
-    for(int i = 0; i < v[topid].size(); i++) {
-      int nextid = v[topid][i];
-      if(visit[nextid] == false && top.layer < l) {
-        node next;
-        next.id = nextid;
-        next.layer = top.layer + 1;
-        q.push(next);
-        visit[nextid] = true;
-        count++;
+int n, k;
+const int inf = 99999999;
+int e[205][205], weight[205], dis[205];
+bool visit[205];
+vector<int> pre[205], temppath, path;
+map<string, int> m;
+map<int, string> m2;
+int maxvalue = 0, mindepth, countpath = 0;
+double maxavg;
+void dfs(int v) {
+  temppath.push_back(v);
+  if(v == 1) {
+    int value = 0;
+    for(int i = 0; i < temppath.size(); i++) {
+      value += weight[temppath[i]];
+    }
+    double tempavg = 1.0 * value / (temppath.size() - 1);
+    if(value > maxvalue) {
+      maxvalue = value;
+      maxavg = tempavg;
+      path = temppath;
+    } else if(value == maxvalue && tempavg > maxavg) {
+      maxavg = tempavg;
+      path = temppath;
+    }
+    temppath.pop_back();
+    countpath++;
+    return ;
+  }
+  for(int i = 0; i < pre[v].size(); i++)
+    dfs(pre[v][i]);
+  temppath.pop_back();
+}
+int main() {
+  fill(e[0], e[0] + 205 * 205, inf);
+  fill(dis, dis + 205, inf);
+  fill(visit, visit + 205, false);
+  scanf("%d %d", &n, &k);
+  string s;
+  cin >> s;
+  m[s] = 1;
+  m2[1] = s;
+  for(int i = 1; i < n; i++) {
+    cin >> s >> weight[i + 1];
+    m[s] = i + 1;
+    m2[i + 1] = s;
+  }
+  string sa, sb;
+  int temp;
+  for(int i = 0; i < k; i++) {
+    cin >> sa >> sb >> temp;
+    e[m[sa]][m[sb]] = temp;
+    e[m[sb]][m[sa]] = temp;
+  }
+  dis[1] = 0;
+  for(int i = 1; i <= n; i++) {
+    int u = -1, minn = inf;
+    for(int j = 1; j <= n; j++) {
+      if(visit[j] == false && dis[j] < minn) {
+        u = j;
+        minn = dis[j];
+      }
+    }
+    if(u == -1) break;
+    visit[u] = true;
+    for(int v = 1; v <= n; v++) {
+      if(visit[v] == false && e[u][v] != inf) {
+        if(dis[u] + e[u][v] < dis[v]) {
+          dis[v] = dis[u] + e[u][v];
+          pre[v].clear();
+          pre[v].push_back(u);
+        } else if(dis[u] + e[u][v] == dis[v]) {
+          pre[v].push_back(u);
+        }
       }
     }
   }
-  return count;
-}
-int main() {
-  scanf("%d %d", &n, &l);
-  v.resize(n + 1);
-  int temp, k;
-  for(int i = 1; i <= n; i++) {
-    scanf("%d", &k);
-    for(int j = 0; j < k; j++) {
-      scanf("%d", &temp);
-      v[temp].push_back(i);
-    }
+  int rom = m["ROM"];
+  dfs(rom);
+  printf("%d %d %d %d\n", countpath, dis[rom], maxvalue, (int)maxavg);
+  for(int i = path.size() - 1; i >= 1; i--) {
+    cout << m2[path[i]] << "->";
   }
-  int m, t;
-  scanf("%d", &m);
-  for(int i = 0; i < m; i++) {
-    scanf("%d", &t);
-    node tnode;
-    tnode.id = t;
-    tnode.layer = 0;
-    int ans = dfs(tnode);
-    printf("%d\n", ans);
-  }
+  cout << "ROM";
   return 0;
 }
