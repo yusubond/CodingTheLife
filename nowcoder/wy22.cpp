031d2c5602825ae882f6ae2b0cff10cc83378b73
Bangchao Yu <yubc0321@gmail.com>
2017-03-24 14:25:16 +0800

    update

--- nowcoder/wy22.cpp
+++ nowcoder/wy22.cpp
@@ -1,16 +1,25 @@
-#include <ctsdio>
+#include <cstdio>
 #include <vector>
 using namespace std;
 vector<int> v;
 int n;
 int main() {
+  int sum = 0, count = 0;
+  long long int s = 1;
   scanf("%d", &n);
   v.resize(n);
   for(int i = 0; i < n; i++)
     scanf("%d", &v[i]);
-  int count;
-  sort(v.begin(), v.end());
-  for(int i = 0; i < v.size(); i++)
-    printf("%d", v[i]);
+  for(int i = 0; i < n; i++) {
+    int sum = 0;
+    long long int s = 1;
+    for(int j = i; j < n; j++) {
+      sum += v[i];
+      s *= v[i];
+      if(sum > s)
+        count++;
+    }
+  }
+  printf("%d", count);
   return 0;
 }
